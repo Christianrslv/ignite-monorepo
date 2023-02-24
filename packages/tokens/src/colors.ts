0e527e9efcb5c3c7819d34c41c5013578a03ba8e
Christianrslv <christianrslv22@gmail.com>
2023-02-24 19:27:49 -0300

    Create monorepo

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray280: '#A9A9B2',
+  gray480: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray780: '#29292E',
+  gray800: '#202024',
+  gray980: '#121214',
+
+  ignite380: '#00B37E',
+  ignite500: '#00875F',
+  ignite700: '#015F43',
+  ignite980: '#00291D',
+}
